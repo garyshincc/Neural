7830cec45eaf1f59f62749f7a4295f69a4c0915b
Garyshincc <garyshincc@gmail.com>
2016-12-18 10:42:53 -0500

    fix on one_neuron

--- one_neuron.py
+++ one_neuron.py
@@ -1,18 +1,26 @@
 import numpy as np
 import random
-class Neuron():
-	def __init__:
-		weight = random.randint(-1, 1)
-		bias = random.randint(a, b)
 
-	def propagate(input):
-		linear = np.dot(input, self.W1)
+class Neuron():
+	def __init__(self):
+		self.weight = random.uniform(-1, 1)
+		self.bias = random.uniform(-1, 1)
+
+	def propagate(self,_input):
+		_input = _input / 100.0
+		print "adjusted input: " + str(_input)
+		linear = np.dot(_input, self.weight)
 		# node
-		non_linear = self.sigmoid(self.z2)
+		print "linear part: " + str(linear)
+		non_linear = self.sigmoid(linear)
+
 		return non_linear
+
+	def sigmoid(self, z):
+		return 1/(1+np.exp(-z))
 
 myNeuron = Neuron()
 
-print myNeuron.propagate(10)
+print myNeuron.propagate(50)
 
 
