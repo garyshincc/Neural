00189975700e5d09bd5fe3aa7bdd5d7c35ad7005
Gary <Garyshincc@gmail.com>
2017-03-11 11:48:44 -0500

    added another ferature for piecewise guided descent

--- TensorFlow/ppl_animate.py
+++ TensorFlow/ppl_animate.py
@@ -48,6 +48,35 @@ class Sine():
 
         return mutated_sine
 
+    def mutate_freq(self, max_freq_cost, cost):
+        cost = cost / (max_freq_cost * math.log(max_freq_cost))
+        l = 1 - cost
+        h = 1 + cost
+
+        mutated_sine = Sine()
+        freq_rand = random.uniform(l,h)
+        mutated_sine.freq = self.freq * freq_rand
+        mutated_sine.amp = self.amp
+
+        wf = (2 * np.pi * self.freq * self.x / self.period) 
+        mutated_sine.y = self.amp * np.sin(wf)
+
+        return mutated_sine
+
+    def mutate_amp(self, max_amp_cost, cost):
+        cost = cost / (max_amp_cost * math.log(max_amp_cost))
+        l = 1 - cost
+        h = 1 + cost
+
+        mutated_sine = Sine()
+        amp_rand = random.uniform(l,h)
+        mutated_sine.amp = self.amp * amp_rand
+        mutated_sine.freq = self.freq
+
+        wf = (2 * np.pi * self.freq * self.x / self.period) 
+        mutated_sine.y = self.amp * np.sin(wf)
+
+        return mutated_sine
 
     def get_freq(self):
         return self.freq
@@ -102,7 +131,7 @@ class MakeBetterBabies():
 
         max_cost = cost_function(self.ugly_duck, self.beautiful_swan)
 
-        while(curr_cost > 0.005):
+        while(num_runs < 200 and total_runs < 10000):
             num_runs = num_runs + 1
             total_runs = total_runs + 1
             next_baby = self.ugly_duck.heuristical_mutate(max_cost, 0, curr_cost)
@@ -119,8 +148,67 @@ class MakeBetterBabies():
             else:
                 num_runs = num_runs - 1
 
-            if(num_runs > 200 or total_runs > 10000):
+            if (curr_cost < 1e-8):
                 break
+
+        print ("total runs: " + str(total_runs))
+        return num_runs
+
+    def individual_descent(self):
+        curr_cost = cost_function(self.ugly_duck, self.beautiful_swan)
+        num_runs = 0
+        total_runs = 0
+        plt.title("Sine Wave")
+
+        # amplitude first
+        max_amp_cost = amp_cost(self.ugly_duck, self.beautiful_swan)
+        curr_amp_cost = amp_cost(self.ugly_duck, self.beautiful_swan)
+        while(num_runs < 200 and total_runs < 10000):
+            num_runs = num_runs + 1
+            total_runs = total_runs + 1
+            next_baby = self.ugly_duck.mutate_amp(max_amp_cost, curr_amp_cost)
+
+            duck_cost = amp_cost(self.ugly_duck, self.beautiful_swan)
+            baby_cost = amp_cost(next_baby, self.beautiful_swan)
+            if(baby_cost < duck_cost):
+                self.ugly_duck = next_baby
+                plt.clf()
+                plt.plot(self.ugly_duck.x, self.ugly_duck.y, self.beautiful_swan.x, self.beautiful_swan.y)
+                plt.pause(0.01)
+
+                curr_amp_cost = amp_cost(self.ugly_duck, self.beautiful_swan)
+            else:
+                num_runs = num_runs - 1
+
+            if (curr_amp_cost < 1e-8):
+                break
+
+        # freq second
+        print (self.ugly_duck.freq)
+        print (self.ugly_duck.amp)
+        max_freq_cost = freq_cost(self.ugly_duck, self.beautiful_swan)
+        curr_freq_cost = freq_cost(self.ugly_duck, self.beautiful_swan)
+        while(num_runs < 200 and total_runs < 10000):
+            num_runs = num_runs + 1
+            total_runs = total_runs + 1
+            next_baby = self.ugly_duck.mutate_freq(max_freq_cost, curr_freq_cost)
+
+            duck_cost = freq_cost(self.ugly_duck, self.beautiful_swan)
+            baby_cost = freq_cost(next_baby, self.beautiful_swan)
+            if(baby_cost < duck_cost):
+                self.ugly_duck = next_baby
+                plt.clf()
+                plt.plot(self.ugly_duck.x, self.ugly_duck.y, self.beautiful_swan.x, self.beautiful_swan.y)
+                plt.pause(0.01)
+
+                curr_freq_cost = freq_cost(self.ugly_duck, self.beautiful_swan)
+            else:
+                num_runs = num_runs - 1
+
+            if (curr_freq_cost < 1e-8):
+                break
+
+
         print ("total runs: " + str(total_runs))
         return num_runs
 
@@ -130,9 +218,14 @@ def cost_function(ugly_duck, beautiful_swan):
 
     return (freq_cost + amp_cost)
 
+def freq_cost(ugly_duck, beautiful_swan):
+    return abs(ugly_duck.get_freq() - beautiful_swan.get_freq())
+
+def amp_cost(ugly_duck, beautiful_swan):
+    return abs(ugly_duck.get_amp() - beautiful_swan.get_amp())
+
 
 def run():
-
     target_sine_wave = Sine()
     target_sine_wave.freq = 5
     target_sine_wave.amp = 5
@@ -179,11 +272,35 @@ def run_with_heuristic():
     plt.title("Sine Wave")
     plt.show()
 
+def run_with_individual_descent():
+    target_sine_wave = Sine()
+    target_sine_wave.freq = 5
+    target_sine_wave.amp = 5
+    wf = (2 * np.pi * target_sine_wave.freq * target_sine_wave.x / target_sine_wave.period) 
+    target_sine_wave.y = target_sine_wave.amp * np.sin(wf)
+
+    sin1 = Sine()
+
+    incubator = MakeBetterBabies(sin1, target_sine_wave)
+    num_runs = incubator.individual_descent()
+
+    best_bird = incubator.ugly_duck
+    print ("best offspring from " + str(num_runs) + " children: ")
+    print ("frequency: " + str(best_bird.freq))
+    print ("amplitude: " + str(best_bird.amp))
+    print ("with cost: " + str(cost_function(best_bird,target_sine_wave)))
+
+
+    plt.plot(best_bird.x, best_bird.y)
+    plt.title("Sine Wave")
+    plt.show()
+
 print ("running with normal stochastic descent")
 run()
 print ("running with heuristic descent")
 run_with_heuristic()
-
+print ("running with individual guided descent")
+run_with_individual_descent()
 
 
 
